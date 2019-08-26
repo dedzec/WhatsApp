202683ba54390039113b7d8c318828b968f5b45a
dedzec <dedzec@gmail.com>
2019-08-26 01:07:21 -0300

    máscaras para telefone

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Projeto WhatsApp (Clone)
 
 Aplicativo desenvolvido do zero no Curso de [Desenvolvimento Android](https://www.udemy.com/course/curso-completo-do-desenvolvedor-android/) disponível na plataforma da Udemy e no site do [jamiltondamasceno.com.br](http://jamiltondamasceno.com.br/).
+
+Componentes Ultilizados:
+* [Material Design](https://www.google.com/design/spec/material-design/introduction.html) - é uma linguagem visual que sintetiza os princípios clássicos do bom design com a inovação da tecnologia e da ciência.
+* [MaskFormatter](https://github.com/rtoshiro/MaskFormatter) - Uma biblioteca do Android para formatar as máscaras de uso do Strings. Pode ser usado com o TextWatcher. (A versão mínima do SDK é 7.)
--- app/src/main/java/com/lucas/whatsappandroid/whatsapp/LoginActivity.java
+++ app/src/main/java/com/lucas/whatsappandroid/whatsapp/LoginActivity.java
@@ -2,14 +2,40 @@ package com.lucas.whatsappandroid.whatsapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText nome;
+    private EditText telefone;
+    private EditText codPais;
+    private EditText codArea;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        nome        = (EditText) findViewById(R.id.edit_nome);
+        telefone    = (EditText) findViewById(R.id.edit_telefone);
+        codPais     = (EditText) findViewById(R.id.edit_cod_pais);
+        codArea     = (EditText) findViewById(R.id.edit_cod_area);
+
+        /*Definir Mascaras*/
+        SimpleMaskFormatter simpleMaskCodPais = new SimpleMaskFormatter("+NN");
+        SimpleMaskFormatter simpleMaskCodArea = new SimpleMaskFormatter("NN");
+        SimpleMaskFormatter simpleMaskTelefone = new SimpleMaskFormatter("NNNNN-NNNN");
+
+        MaskTextWatcher maskCodPais = new MaskTextWatcher(codPais, simpleMaskCodPais);
+        MaskTextWatcher maskCodArea = new MaskTextWatcher(codArea, simpleMaskCodArea);
+        MaskTextWatcher maskTelefone = new MaskTextWatcher(telefone, simpleMaskTelefone);
+
+        codPais.addTextChangedListener(maskCodPais);
+        codArea.addTextChangedListener(maskCodArea);
+        telefone.addTextChangedListener(maskTelefone);
 
     }
 }
