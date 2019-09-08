3236fdfecde5743b63126594f31ce1164133050f
dedzec <dedzec@gmail.com>
2019-09-08 00:30:48 -0300

    Deslogar usuário e configurações

--- app/src/main/java/com/lucas/whatsappandroid/whatsapp/activity/MainActivity.java
+++ app/src/main/java/com/lucas/whatsappandroid/whatsapp/activity/MainActivity.java
@@ -3,9 +3,10 @@ package com.lucas.whatsappandroid.whatsapp.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -15,34 +16,27 @@ import com.lucas.whatsappandroid.whatsapp.config.ConfiguracaoFirebase;
 public class MainActivity extends AppCompatActivity {
 
     private Button botaoSair;
-    private FirebaseAuth autenticacao;
+    private FirebaseAuth usuarioAutenticacao;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        botaoSair = (Button) findViewById(R.id.bt_sair);
+        usuarioAutenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
 
-        botaoSair.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
-                autenticacao.signOut();
-
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-
-            }
-        });
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("WhatsApp");
+        setSupportActionBar( toolbar );
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -51,13 +45,22 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.item_sair:
+                deslogarUsuario();
+                return true;
+            case R.id.item_configuracoes:
+                return  true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    public void deslogarUsuario(){
+        usuarioAutenticacao.signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
